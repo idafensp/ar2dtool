059a6aaaa482e3f32520cdf95acc5c3abddf6672
Idafen Santana Pérez <idafensp@gmail.com>
2016-09-19 16:03:06 +0200

    max num triples added to parseArgs

--- mavenProject/src/main/java/es/upm/oeg/ar2dtool/Main.java
+++ mavenProject/src/main/java/es/upm/oeg/ar2dtool/Main.java
@@ -1,5 +1,18 @@
 package es.upm.oeg.ar2dtool;
 
+/*
+ * Copyright 2012-2013 Ontology Engineering Group, Universidad Politécnica de Madrid, Spain
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
+with the License. You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under 
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+
 import java.util.logging.Level;
 
 import es.upm.oeg.ar2dtool.exceptions.ConfigFileNotFoundException;
@@ -13,11 +26,12 @@ import es.upm.oeg.ar2dtool.utils.graphml.GraphMLGenerator;
 public class Main {
 
 	private static final int ARG_LENGTH = 8;
-	public static String syntaxErrorMsg = "Syntax error. Please use the following syntax \"java -jar ar2dtool.jar -i PathToInputRdfFile -o FileToOutputFile -t OutputFileType -c PathToConfFile [-d]\"";
+	public static String syntaxErrorMsg = "Syntax error. Please use the following syntax \"java -jar ar2dtool.jar -i PathToInputRdfFile -o FileToOutputFile -t OutputFileType -c PathToConfFile [-d] [-mnt MaxNumberOfTriples]\"";
 	private static String pathToInputFile = "";
 	private static String pathToOuputFile = "";
 	private static String outputFileType = ""; 
 	private static String pathToConfFile = "";
+	private static int maxNumberOfTriples = 1000;
 
 	private static boolean DEBUG = false;
 
@@ -198,9 +212,16 @@ public class Main {
 										}
 										else
 										{
-
-											dbg(syntaxErrorMsg,Level.WARNING);
-											return;	
+											if(args[i].equals("-mnt"))
+											{
+												i++;
+												maxNumberOfTriples = Integer.valueOf(args[i]);
+											}
+											else
+											{
+												dbg(syntaxErrorMsg,Level.WARNING);
+												return;	
+											}
 										}
 									}
 								}
